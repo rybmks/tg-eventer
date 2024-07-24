5857f4bf9826168309fe13bfacffac9cd5d7a045
rybmks <rybalko.maksym1@lll.kpi.ua>
2024-07-24 13:48:44 +0300

    added dictionary with commands

---
+++ bot/Commands/ComandsHandler.cs
@@ -0,0 +1,29 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace bot
+{
+    public class ComandsHandler
+    {
+        private ITelegramBotClient botClient;
+        public ComandsHandler(ITelegramBotClient telegramBotClient)
+        {
+            this.botClient = telegramBotClient;
+        }
+
+        public static readonly Dictionary<BotCommand, Func<long, Task>> commands = new Dictionary<BotCommand, Func<long, Task>>
+            {
+                {new BotCommand {Command = "start", Description = "Starting bot" }, StartCommand},
+            };
+
+        public async Task SetCommands()
+        {
+            await botClient.SetMyCommandsAsync(commands.Keys);
+        }
+        
+        private static async Task StartCommand(long chatId)
+        {
+            await Program.botClient.SendTextMessageAsync(chatId, "Welcome! Use /help to see available commands.");
+        }
+    }
+}
